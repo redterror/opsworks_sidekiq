df1e1815b8c50446dd3fa3f84dedfdb26c594fa2
Dave Steinberg <dave.steinberg@hkstrategies.com>
2015-02-11 16:27:20 +0000

    support overriding the start command for sidekiq

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,6 +5,7 @@ default[:sidekiq] = {}
 node[:deploy].each do |application, deploy|
   default[:sidekiq][application.intern] = {}
   default[:sidekiq][application.intern][:restart_command] = "sudo monit restart -g sidekiq_#{application}_group"
-  default[:sidekiq][application.intern][:syslog] = false
+  default[:sidekiq][application.intern][:syslog]          = false
+  default[:sidekiq][application.intern][:exec_command]    = "bundle exec sidekiq"
 end
 
--- recipes/setup.rb
+++ recipes/setup.rb
@@ -68,7 +68,8 @@ node[:deploy].each do |application, deploy|
         :deploy => deploy,
         :application => application,
         :workers => workers,
-        :syslog => node[:sidekiq][application][:syslog]
+        :syslog => node[:sidekiq][application][:syslog],
+        :exec_command => node[:sidekiq][application][:exec_command]
       })
       notifies :reload, resources(:service => "monit"), :immediately
     end
